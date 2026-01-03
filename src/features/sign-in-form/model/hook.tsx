89b324558bfbe11d883439824da1180b19c81cdd
mr-mashanlo <mr.mashanlo@yandex.ru>
2026-01-03 23:36:26 +0500

    Add sign in form

---
+++ src/features/sign-in-form/model/hook.tsx
@@ -0,0 +1,38 @@
+import { useForm } from '@tanstack/react-form';
+import { HTTPError } from 'ky';
+
+import { useAuth } from '@/entities/auth';
+import { mapServerErrors } from '@/shared/mappers';
+
+interface Props {
+  onSuccess?: () => void,
+  onError?: () => void
+}
+
+const useSignIn = ( { onSuccess, onError }: Props = {} ) => {
+  const { signin } = useAuth();
+
+  const form = useForm( {
+    defaultValues: {
+      nickname: '',
+      password: ''
+    },
+
+    onSubmit: async ( { value, formApi } ) => {
+      try {
+        await signin( value );
+        onSuccess?.();
+      } catch ( error ) {
+        if ( error instanceof HTTPError ) {
+          const errors = await error.response.json();
+          formApi.setErrorMap( { onChange: { fields: mapServerErrors( errors.issues ) } } );
+        }
+        onError?.();
+      }
+    }
+  } );
+
+  return form;
+};
+
+export default useSignIn;
