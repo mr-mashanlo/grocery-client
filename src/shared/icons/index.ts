8df6b681d7592ad6a8a95ff3db708f96a1ac8e49
mr-mashanlo <mr.mashanlo@yandex.ru>
2026-01-03 23:32:00 +0500

    Add icons

---
+++ src/shared/icons/index.ts
@@ -0,0 +1,13 @@
+import ClosedEyeIcon from './closed-eye';
+import EmailIcon from './email';
+import LockIcon from './lock';
+import OpenEyeIcon from './open-eye';
+import WarningIcon from './warning';
+
+export {
+  ClosedEyeIcon,
+  EmailIcon,
+  LockIcon,
+  OpenEyeIcon,
+  WarningIcon
+};
