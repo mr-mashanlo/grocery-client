48882e71f33841a1ae8c838797800861ea30eb23
mr-mashanlo <mr.mashanlo@yandex.ru>
2026-01-07 23:53:57 +0500

    Refactor products widget

--- src/widgets/products/ui/products.tsx
+++ src/widgets/products/ui/products.tsx
@@ -1,14 +1,18 @@
 import { type FC } from 'react';
 
-import { ProductCard, useProducts } from '@/entities/product';
+import { ProductCard, useFilterStore, useProducts } from '@/entities/product';
 import { CartControls } from '@/features/cart-controls';
 
 const Products: FC = () => {
-  const { products } = useProducts( { limit: '0' } );
+  const { category } = useFilterStore();
+  const { products } = useProducts( { limit: '0', category: category._id } );
 
   return (
-    <section className="max-w-200 mx-auto  grid sm:grid-cols-2 gap-3 sm:gap-5">
-      {products.data?.data.map( product => <ProductCard key={product._id} product={product} controls={<CartControls product={product._id} />} /> )}
+    <section className="max-w-200 mx-auto">
+      <h2 className="mb-15 text-center text-2xl font-bold">{category.title}</h2>
+      <div className="grid sm:grid-cols-2 gap-3 sm:gap-5">
+        {products.data?.data.map( product => <ProductCard key={product._id} product={product} controls={<CartControls product={product._id} />} /> )}
+      </div>
     </section>
   );
 };
