1d03d0f12c4f639fd18aa16c90d291741f6fd86a
mr-mashanlo <mr.mashanlo@yandex.ru>
2026-01-07 14:39:13 +0500

    Update home page

--- src/pages/home/ui/page.tsx
+++ src/pages/home/ui/page.tsx
@@ -1,5 +1,7 @@
 import { type FC } from 'react';
 
+import { Products } from '@/widgets/products';
+
 export const HomePage: FC = () => {
   return (
     <>
@@ -8,6 +10,10 @@ export const HomePage: FC = () => {
       <meta property="og:image" content="/meta.svg" />
       <meta property="og:site_name" content="Grocery" />
       <meta property="twitter:card" content="summary" />
+
+      <div className="p-3 sm:p-15">
+        <Products />
+      </div>
     </>
   );
 };
