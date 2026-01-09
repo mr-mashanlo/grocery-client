c1a256cc49df17e5a91f4e4090628450a93c3f43
mr-mashanlo <mr.mashanlo@yandex.ru>
2026-01-09 13:16:15 +0500

    Refactor shop controls

--- src/features/shop-controls/ui/shop-controls.tsx
+++ src/features/shop-controls/ui/shop-controls.tsx
@@ -8,13 +8,13 @@ import { useFilterStore } from '@/entities/product';
 
 const ShopControls: FC = () => {
   const { getQuantities } = useCartStore();
-  const { categories } = useCategories( { limit: '0' } );
+  const { categories } = useCategories( { limit: '0', archived: 'false' } );
   const { category, setCategory } = useFilterStore();
 
   return (
-    <div className="fixed bottom-0 left-0 flex items-center">
+    <div className="w-full sm:w-150 grid grid-cols-4 items-center fixed bottom-0 left-0">
       <Listbox value={category} onChange={setCategory}>
-        <ListboxButton className="block w-55 sm:w-70 p-5 text-left bg-zinc-100 cursor-pointer outline-0">
+        <ListboxButton className="p-5 col-span-2 text-left bg-zinc-100 cursor-pointer outline-0">
           {category.title}
         </ListboxButton>
         <ListboxOptions anchor="top" transition className="w-(--button-width) p-5 flex flex-col gap-5 bg-zinc-100 transition duration-100 ease-in data-leave:data-closed:opacity-0 outline-0">
@@ -22,7 +22,8 @@ const ShopControls: FC = () => {
           {categories.data?.data.map( category => <ListboxOption key={category._id} value={category} className="group flex items-center gap-2 cursor-pointer">{category.title}</ListboxOption> )}
         </ListboxOptions>
       </Listbox>
-      <Link to="/cart" className="w-40 sm:w-50 p-5 text-center bg-zinc-200 cursor-pointer">Cart ({getQuantities()})</Link>
+      <Link to="/cart" className="py-5 text-center bg-zinc-200 cursor-pointer">Cart ({getQuantities()})</Link>
+      <Link to="/orders" className="py-5 text-center bg-zinc-300 cursor-pointer">Orders</Link>
     </div>
   );
 };
