8de43a62f2276f59b297c179eed7967df935f40b
mr-mashanlo <mr.mashanlo@yandex.ru>
2026-01-10 17:04:31 +0500

    Update router file

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,9 +2,11 @@ import { createBrowserRouter } from 'react-router';
 
 import { addressRouter } from '@/pages/address';
 import { cartRouter } from '@/pages/cart';
+import { checkoutRouter } from '@/pages/checkout';
 import { homeRouter } from '@/pages/home';
 import { notFoundRouter } from '@/pages/not-found';
-import { ordersRouter } from '@/pages/order';
+import { orderRouter } from '@/pages/order';
+import { ordersRouter } from '@/pages/orders';
 import { signInRouter } from '@/pages/sign-in';
 import { signUpRouter } from '@/pages/sign-up';
 
@@ -16,7 +18,7 @@ const router = createBrowserRouter( [
     children: [
       {
         element: <MainLayout />,
-        children: [ signInRouter, signUpRouter, homeRouter, cartRouter, addressRouter, ordersRouter, notFoundRouter ]
+        children: [ signInRouter, signUpRouter, homeRouter, cartRouter, checkoutRouter, addressRouter, ordersRouter, orderRouter, notFoundRouter ]
       }
     ]
   }
