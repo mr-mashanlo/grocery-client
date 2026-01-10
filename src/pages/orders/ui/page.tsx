419da5b4cea808d645a96a3c0a42e453d9032803
mr-mashanlo <mr.mashanlo@yandex.ru>
2026-01-10 17:03:25 +0500

    Add orders page

---
+++ src/pages/orders/ui/page.tsx
@@ -0,0 +1,26 @@
+import { type FC } from 'react';
+import { Link } from 'react-router';
+
+import { OrderProducts } from '@/widgets/order-products';
+
+export const OrdersPage: FC = () => {
+  return (
+    <>
+      <title>Grocery</title>
+      <meta property="og:title" content="Grocery" />
+      <meta property="og:image" content="/meta.svg" />
+      <meta property="og:site_name" content="Grocery" />
+      <meta property="twitter:card" content="summary" />
+
+      <div className="px-4 py-10 pb-19 sm:p-37.5">
+        <OrderProducts />
+        <div className="w-full sm:w-150 grid grid-cols-3 items-center fixed bottom-0 left-0">
+          <div className="h-full col-span-2 bg-zinc-100"></div>
+          <Link to="/" className="py-5 text-center bg-black text-white">Back</Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OrdersPage;
