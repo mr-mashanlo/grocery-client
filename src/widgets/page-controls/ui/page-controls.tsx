8531ebc9b9181e91ab42a23a91050d0e97476db4
mr-mashanlo <mr.mashanlo@yandex.ru>
2026-01-10 16:54:46 +0500

    Refactor page controls

--- src/widgets/page-controls/ui/page-controls.tsx
+++ src/widgets/page-controls/ui/page-controls.tsx
@@ -1,6 +1,6 @@
 import { type FC, type ReactNode } from 'react';
 
-import { useCartStore } from '@/entities/cart';
+import { getTotalPrice, useCartStore } from '@/entities/cart';
 import { useProducts } from '@/entities/product';
 
 interface Props {
@@ -11,14 +11,11 @@ const PageControls: FC<Props> = ( { controls } ) => {
   const { products: cartProducts, getQuantities } = useCartStore();
   const { products: shopProducts } = useProducts( { limit: '0' } );
 
-  const total = cartProducts.reduce( ( acc, cartProduct ) => {
-    const shopProduct = shopProducts.data?.data.find( shopProduct => cartProduct.product === shopProduct._id );
-    return acc = acc + cartProduct.quantity * ( shopProduct?.price || 0 );
-  }, 0 );
+  const total = getTotalPrice( cartProducts, shopProducts.data?.data || [] );
 
   return (
-    <div className="w-full sm:w-150 grid grid-cols-4 items-center fixed bottom-0 left-0">
-      <p className="p-5 col-span-3 sm:col-span-2 text-left bg-zinc-100">Total: ${total}, Qty: {getQuantities()}</p>
+    <div className="w-full sm:w-150 grid grid-cols-3 items-center fixed bottom-0 left-0">
+      <p className="p-5 col-span-2 sm:col-span-2 text-left bg-zinc-100">Total: {total}, Quantity: {getQuantities()}</p>
       {controls}
     </div>
   );
