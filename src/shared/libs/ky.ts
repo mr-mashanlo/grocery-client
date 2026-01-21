ebd677740d351e9d08f410caa2aa3d02168a69e4
mr-mashanlo <mr.mashanlo@yandex.ru>
2026-01-21 19:34:09 +0500

    Refactor ky instance

--- src/shared/libs/ky.ts
+++ src/shared/libs/ky.ts
@@ -8,7 +8,7 @@ export const kyInstance = ky.create( {
   headers: { 'Content-Type': 'application/json' },
   retry: {
     limit: 1,
-    statusCodes: [ 499, 400 ],
+    statusCodes: [ 498, 499, 400 ],
     methods: [ 'get', 'post', 'put', 'delete' ]
   },
   hooks: {
@@ -16,8 +16,12 @@ export const kyInstance = ky.create( {
     afterResponse: [
       async ( _request, _options, _response ) => {
         try {
-          if ( _response.status !== 499 ) return;
-          await ky.get( `${import.meta.env.VITE_BACK_URL}/auth/refresh`, { credentials: 'include', headers: { 'Content-Type': 'application/json' } } );
+          if ( _response.status === 498 ) {
+            throw new UnauthorizedError();
+          }
+          if ( _response.status === 499 ) {
+            await ky.get( `${import.meta.env.VITE_BACK_URL}/auth/refresh`, { credentials: 'include' } );
+          }
         } catch {
           throw new UnauthorizedError();
         }
