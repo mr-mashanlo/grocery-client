b8055604d018f5e72c2f201dd2016e15f7a65085
mr-mashanlo <mr.mashanlo@yandex.ru>
2026-04-23 01:01:53 +0500

    Refactor main.js

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -6,7 +6,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 
-import router from './router';
+import { router } from './routes/router';
 
 export const queryClient = new QueryClient();
 
