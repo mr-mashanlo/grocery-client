e830c32be2e0c14410908c574526a33ed1b55b3f
mr-mashanlo <mr.mashanlo@yandex.ru>
2026-04-24 14:14:14 +0500

    Add particles

--- src/features/cart/add-to-cart-button/ui/form.tsx
+++ src/features/cart/add-to-cart-button/ui/form.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@headlessui/react';
+import confetti from 'canvas-confetti';
 import { type ButtonHTMLAttributes, type DetailedHTMLProps, type FC } from 'react';
 
 import { type CreateCartDTO, useCartStore } from '@/entities/cart';
@@ -10,8 +11,19 @@ interface Props extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement
 const AddToCartButton: FC<Props> = ( { product, ...props } ) => {
   const increase = useCartStore( state => state.increase );
 
+  const handleButtonClick = () => {
+    increase( product );
+    confetti( {
+      scalar: 2,
+      spread: 180,
+      particleCount: 60,
+      origin: { y: -0.1 },
+      startVelocity: -35
+    } );
+  };
+
   return (
-    <Button onClick={() => increase( product )} {...props} />
+    <Button onClick={handleButtonClick} {...props} />
   );
 };
 
