b161a52fdde760984b5c521e13d65b4c0a70cad2
mr-mashanlo <mr.mashanlo@yandex.ru>
2026-05-03 00:42:55 +0500

    Remove limits from ky instance

--- src/shared/libs/ky.ts
+++ src/shared/libs/ky.ts
@@ -5,12 +5,10 @@ export class UnauthorizedError extends Error {}
 export const kyInstance = ky.create( {
   prefixUrl: import.meta.env.VITE_BACK_URL,
   credentials: 'include',
-  headers: { 'Content-Type': 'application/json' },
-  retry: { limit: 1 }
+  headers: { 'Content-Type': 'application/json' }
 } );
 
 export const kyMediaInstance = ky.create( {
   prefixUrl: import.meta.env.VITE_BACK_URL,
-  credentials: 'include',
-  retry: { limit: 1 }
+  credentials: 'include'
 } );
