50afe04e8a644ec036c217ca4533e5b0c2a91dfc
mr-mashanlo <mr.mashanlo@yandex.ru>
2026-05-03 12:01:50 +0500

    Detete all skeletons from widgets

--- src/widgets/category/grid/ui/widget.tsx
+++ src/widgets/category/grid/ui/widget.tsx
@@ -2,31 +2,21 @@ import { type DetailedHTMLProps, type FC, type HTMLAttributes } from 'react';
 import { Link, useSearchParams } from 'react-router';
 import { twMerge } from 'tailwind-merge';
 
-import { type PaginatedCategory, useCategories } from '@/entities/category';
+import { useCategories } from '@/entities/category';
 
 import Header from './header';
 
-interface Props extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
-  categories?: PaginatedCategory
-}
+type Props = DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>;
 
-const Skeleton: FC<Props> = ( { className, ...props } ) => {
-  return (
-    <section className={twMerge( 'mx-4 my-8 sm:mx-10 sm:my-20', className )} {...props}>
-      <Header />
-      <ul className="grid grid-cols-2 sm:grid-cols-3 gap-4 sm:gap-5">
-        {Array.from( { length: 6 } ).map( ( _, index ) => <li key={index} className="w-full aspect-square bg-zinc-100 rounded-xl"></li> )}
-      </ul>
-    </section>
-  );
-};
+const CategoryGrid: FC<Props> = ( { className, ...props } ) => {
+  const [ searchParams ] = useSearchParams();
+  const { categories } = useCategories( { archived: 'false', ...Object.fromEntries( searchParams.entries() ) } );
 
-const Widget: FC<Props> = ( { className, categories, ...props } ) => {
   return (
     <section className={twMerge( 'mx-4 my-8 sm:mx-10 sm:my-20', className )} {...props}>
       <Header />
       <ul className="grid sm:grid-cols-3 gap-4 sm:gap-5">
-        {categories?.data.map( category =>
+        {categories.data?.data.map( category =>
           <li key={category._id} className="relative">
             <Link to={`/products?category=${category.slug}`}><img src={category.image.url} alt={category.image.alt} className="rounded-xl" /></Link>
             <h3 className="absolute bottom-5 left-5">{category.name}</h3>
@@ -35,12 +25,6 @@ const Widget: FC<Props> = ( { className, categories, ...props } ) => {
       </ul>
     </section>
   );
-};
-
-const CategoryGrid: FC<Props> = () => {
-  const [ searchParams ] = useSearchParams();
-  const { categories } = useCategories( { archived: 'false', ...Object.fromEntries( searchParams.entries() ) } );
-  return categories.isLoading ? <Skeleton /> : <Widget categories={categories.data} />;
 };
 
 export default CategoryGrid;
--- src/widgets/product/grid/ui/widget.tsx
+++ src/widgets/product/grid/ui/widget.tsx
@@ -2,33 +2,22 @@ import { type DetailedHTMLProps, type FC, type HTMLAttributes } from 'react';
 import { Link, useSearchParams } from 'react-router';
 import { twMerge } from 'tailwind-merge';
 
-import { type PaginatedProduct, useProducts } from '@/entities/product';
+import { useProducts } from '@/entities/product';
 
 import Header from './header';
 import Pagination from './pagination';
 
-interface Props extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
-  products?: PaginatedProduct
-}
+type Props = DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>;
 
-const Skeleton: FC<Props> = ( { className, ...props } ) => {
-  return (
-    <section className={twMerge( 'mx-4 my-8 sm:mx-10 sm:my-20 animate-pulse', className )} {...props}>
-      <Header />
-      <ul className="grid grid-cols-2 sm:grid-cols-3 gap-4 sm:gap-5">
-        {Array.from( { length: 6 } ).map( ( _, index ) => <li key={index} className="w-full aspect-square bg-zinc-200 rounded-xl"></li> )}
-      </ul>
-      <Pagination limit={0} page={0} total={0} />
-    </section>
-  );
-};
+const ProductGrid: FC<Props> = ( { className, ...props } ) => {
+  const [ searchParams ] = useSearchParams();
+  const { products } = useProducts( { limit: '9', archived: 'false', ...Object.fromEntries( searchParams.entries() ) } );
 
-const Widget: FC<Props> = ( { className, products, ...props } ) => {
   return (
     <section className={twMerge( 'mx-4 my-8 sm:mx-10 sm:my-20', className )} {...props}>
       <Header />
       <ul className="grid grid-cols-2 sm:grid-cols-3 gap-4 sm:gap-5">
-        {products?.data.map( product =>
+        {products.data?.data.map( product =>
           <li key={product._id} className="relative">
             <Link to={`/products/${product.slug}`} className="block rounded-xl overflow-hidden"><img src={product.images[0].url} alt={product.images[0].alt} className="w-full h-full aspect-square" /></Link>
             <h3 className="absolute bottom-5 left-5">{product.name}</h3>
@@ -36,15 +25,9 @@ const Widget: FC<Props> = ( { className, products, ...props } ) => {
           </li>
         )}
       </ul>
-      <Pagination limit={products?.limit || 0} page={products?.page || 0} total={products?.total || 0} />
+      <Pagination limit={products.data?.limit || 0} page={products.data?.page || 0} total={products.data?.total || 0} />
     </section>
   );
-};
-
-const ProductGrid: FC<Props> = () => {
-  const [ searchParams ] = useSearchParams();
-  const { products } = useProducts( { limit: '9', archived: 'false', ...Object.fromEntries( searchParams.entries() ) } );
-  return products.isLoading ? <Skeleton /> : <Widget products={products.data} />;
 };
 
 export default ProductGrid;
